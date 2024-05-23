49cc9080c32f258bd554e48fa78a0281dcb72818
Eddie110102 <eddiewang@ispan.com.tw>
2024-05-23 16:41:44 +0800

    第一支 express

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 
-console.log(process.env.DB_HOST);
-console.log(process.env.DB_USER);
+// console.log(process.env.DB_HOST);
+// console.log(process.env.DB_USER);
 
+import express from "express";
+
+const app = express();
+
+// 設定路由, 只允許用 GET 拜訪
+app.get('/', (req, res)=>{
+  res.send(`<h2>哈囉</h2>`);
+});
+
+const port = process.env.WEB_PORT || 3002;
+app.listen(port, ()=>{
+  console.log(`Server start: port ${port}`);
+});
