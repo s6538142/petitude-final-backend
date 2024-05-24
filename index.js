27a2d217da6af4ff5709304792868eb2427789a2
Eddie110102 <eddiewang@ispan.com.tw>
2024-05-24 09:42:31 +0800

    靜態內容資料夾

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ import express from "express";
 
 const app = express();
 
-
 // routes
 // 設定路由, 只允許用 GET 拜訪
 app.get("/", (req, res) => {
   res.send(`<h2>哈囉</h2>`);
 });
 
+// 設定靜態內容資料夾
+app.use(express.static("public"));
 
-// ************ 要放在所有的路由設定之後
+// ************ 404 要放在所有的路由設定之後
 // use 接受所有 HTTP 方法
 app.use((req, res) => {
   res.type("text/plain").status(404).send("走錯路了");
