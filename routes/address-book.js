3da50668290ae6d745b2daf19be9a43827b0f362
Eddie110102 <eddiewang@ispan.com.tw>
2024-05-28 14:14:57 +0800

    總筆數

--- routes/address-book.js
+++ routes/address-book.js
@@ -1,9 +1,14 @@
 import express from "express";
+import db from "./../utils/connect-mysql.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json(req.originalUrl);
+router.get("/", async (req, res) => {
+  const t_sql = "SELECT COUNT(1) totalRows FROM address_book";
+  const [results] = await db.query(t_sql);
+
+
+  res.json(results);
 });
 
 export default router;
