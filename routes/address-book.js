1ef9db888e6d8075a5a21101663d58fb6e7542cc
Eddie110102 <eddiewang@ispan.com.tw>
2024-05-28 14:49:41 +0800

    用戶要看哪一頁 page

--- routes/address-book.js
+++ routes/address-book.js
@@ -4,10 +4,21 @@ import db from "./../utils/connect-mysql.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
+  let success = false;
+
+  const perPage = 25; // 每頁最多有幾筆資料
+  let page = parseInt(req.query.page) || 1; // 從 query string 最得 page 的值
+  if (page < 1) {
+    return res.redirect("?page=1"); // 跳轉頁面
+  }
+
   const t_sql = "SELECT COUNT(1) totalRows FROM address_book";
   const [[{ totalRows }]] = await db.query(t_sql);
+  if(totalRows) {
+    // 取得分頁資料
+  }
 
-  res.json(totalRows);
+  res.json({ success, perPage, page, totalRows });
 });
 
 export default router;
