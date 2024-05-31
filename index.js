bfeb6bc1f0b80e4315d36ae3cb0879ea56f0353e
Eddie110102 <eddiewang@ispan.com.tw>
2024-05-31 15:46:30 +0800

    登入的表單

--- index.js
+++ index.js
@@ -187,6 +187,13 @@ app.get("/yahoo", async (req, res) => {
   res.send(txt);
 });
 
+app.get("/login", async (req, res) => {
+  res.render("login");
+});
+app.post("/login", upload.none(), async (req, res) => {
+  res.json(req.body)
+});
+
 // ************
 // 設定靜態內容資料夾
 app.use(express.static("public"));
