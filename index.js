31e8bf1a995a54d220c806a0785eb71344e9e932
trent3281 <trent3281@gmail.com>
2024-07-15 00:56:22 +0800

    class-page路由

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ import db from "./utils/connect-mysql.js";
 import abRouter from "./routes/address-book.js";
 import articleRouter from "./routes/article-list.js";
 import classRouter from "./routes/class-list.js";
+import APRouter from "./routes/article-page.js";
 import cors from "cors";
 import mysql_session from "express-mysql-session";
 import bcrypt from "bcrypt";
@@ -83,6 +84,7 @@ app.use("/address-book", abRouter);
 //論壇路由
 app.use("/article-list", articleRouter);
 app.use("/class-list", classRouter);
+app.use("/article-page", APRouter);
 
 app.get("/json-sales", (req, res) => {
   const sales = [
